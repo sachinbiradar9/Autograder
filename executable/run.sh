553a285c5155c0a7b9e8feb6732437a42ba82253
Sachin G Biradar <009dollar@gmail.com>
2018-04-18 23:07:35 -0500

    Add files via upload

--- executable/run.sh
+++ executable/run.sh
@@ -1,3 +1,6 @@
 #java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer 9000
 pip install -r requirements.txt
+pip install nltk
+pip install pyenchant
+pip install pattern
 python autograder.py
